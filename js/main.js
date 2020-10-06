a1bb5bfec38046209dbc0b04c9312d752574313b
Dmitrii Shitikov <dshitikov@rtech.ru>
2020-10-06 13:49:37 +0300

    генерация объявлений

---
+++ js/main.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const BUILD_TYPE = [`palace`, `flat`, `house`, `bungalow`];
+const CHECKIN = [`12:00`, `13:00`, `14:00`];
+const CHECKOUT = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const PHOTOS = [
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
+];
+const MAP_CLASS = `map__pins`;
+const MIN_Y = 130;
+const MAX_Y = 630;
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 10;
+const MIN_PRICE = 100;
+const MAX_PRICE = 1000;
+const ADVERTS_COUNT = 8;
+const mapWidth = document.querySelector(`.` + MAP_CLASS).offsetWidth;
+
+let getRandomFromArray = (arr = []) => BUILD_TYPE[Math.floor(Math.random() * arr.length)];
+
+let getRandomInteger = (min = 0, max = 1) => {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+let generateAdverts = () => {
+  let result = [];
+
+  for (let i = 0; i < ADVERTS_COUNT; i++) {
+    let type = getRandomFromArray(BUILD_TYPE);
+    let guests = getRandomInteger(MIN_ROOMS, MAX_ROOMS);
+    let price = getRandomInteger(MIN_PRICE, MAX_PRICE);
+    let location = {
+      x: getRandomInteger(0, mapWidth),
+      y: getRandomInteger(MIN_Y, MAX_Y),
+    };
+
+    let advert = {
+      author: {
+        avatar: `img/avatars/user0${i + 1}.png`,
+      },
+      offer: {
+        title: type,
+        address: `${location.x}, ${location.x}`,
+        price,
+        type: `Сдается ${type}`,
+        rooms: getRandomInteger(1, 10),
+        guests: getRandomInteger(1, 10),
+        checkin: getRandomFromArray(CHECKIN),
+        checkout: getRandomFromArray(CHECKOUT),
+        features: getRandomFromArray(FEATURES),
+        description: `Сдается ${type} на ${guests} гостей за ${price}`,
+        photos: getRandomFromArray(PHOTOS),
+      },
+      location,
+    };
+    result.push(advert);
+  }
+
+  return result;
+};
+
+let createPin = (advert) => {
+  let template = document.querySelector(`#pin`).content.querySelector(`.map__pin`).cloneNode(true);
+  let img = template.getElementsByTagName(`img`)[0];
+  img.setAttribute(`src`, advert.author.avatar);
+  img.setAttribute(`alt`, advert.offer.title);
+  template.setAttribute(`style`, `left: ${advert.location.x}px; top: ${advert.location.y}px`);
+
+  return template;
+};
+
+let createPins = () => {
+  let adverts = generateAdverts();
+  let fragment = document.createDocumentFragment();
+  adverts.forEach((advert) => {
+    fragment.appendChild(createPin(advert));
+  });
+
+  return fragment;
+};
+
+let addAdvertsToMap = () => {
+  let fragment = createPins();
+  document.querySelector(`.` + MAP_CLASS).appendChild(fragment);
+};
+
+/**
+ * Добавление объявлений на карту
+ */
+addAdvertsToMap();
