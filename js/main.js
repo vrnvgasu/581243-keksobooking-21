1134f44619900ae02ee2d9b17b6962c22c7e8ae8
Dmitrii Shitikov <dshitikov@rtech.ru>
2020-10-07 23:21:17 +0300

    генерация объявлений. правки

--- js/main.js
+++ js/main.js
@@ -19,65 +19,67 @@ const MAX_PRICE = 1000;
 const ADVERTS_COUNT = 8;
 const mapWidth = document.querySelector(`.` + MAP_CLASS).offsetWidth;
 
-let getRandomFromArray = (arr = []) => BUILD_TYPE[Math.floor(Math.random() * arr.length)];
+let getRandomFromArray = (arr = []) => arr[Math.floor(Math.random() * arr.length)];
 
-let getRandomInteger = (min = 0, max = 1) => {
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+let getRandomInteger = (min = 0, max = 1) => Math.floor(min + Math.random() * (max + 1 - min));
+
+let createAdvert = (i) => {
+  let type = getRandomFromArray(BUILD_TYPE);
+  let guests = getRandomInteger(MIN_ROOMS, MAX_ROOMS);
+  let price = getRandomInteger(MIN_PRICE, MAX_PRICE);
+  let location = {
+    x: getRandomInteger(0, mapWidth),
+    y: getRandomInteger(MIN_Y, MAX_Y),
+  };
+
+  return {
+    author: {
+      avatar: `img/avatars/user0${i + 1}.png`,
+    },
+    offer: {
+      title: type,
+      address: `${location.x}, ${location.x}`,
+      price,
+      type: `Сдается ${type}`,
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 10),
+      checkin: getRandomFromArray(CHECKIN),
+      checkout: getRandomFromArray(CHECKOUT),
+      features: getRandomFromArray(FEATURES),
+      description: `Сдается ${type} на ${guests} гостей за ${price}`,
+      photos: getRandomFromArray(PHOTOS),
+    },
+    location,
+  };
 };
 
 let generateAdverts = () => {
   let result = [];
 
   for (let i = 0; i < ADVERTS_COUNT; i++) {
-    let type = getRandomFromArray(BUILD_TYPE);
-    let guests = getRandomInteger(MIN_ROOMS, MAX_ROOMS);
-    let price = getRandomInteger(MIN_PRICE, MAX_PRICE);
-    let location = {
-      x: getRandomInteger(0, mapWidth),
-      y: getRandomInteger(MIN_Y, MAX_Y),
-    };
-
-    let advert = {
-      author: {
-        avatar: `img/avatars/user0${i + 1}.png`,
-      },
-      offer: {
-        title: type,
-        address: `${location.x}, ${location.x}`,
-        price,
-        type: `Сдается ${type}`,
-        rooms: getRandomInteger(1, 10),
-        guests: getRandomInteger(1, 10),
-        checkin: getRandomFromArray(CHECKIN),
-        checkout: getRandomFromArray(CHECKOUT),
-        features: getRandomFromArray(FEATURES),
-        description: `Сдается ${type} на ${guests} гостей за ${price}`,
-        photos: getRandomFromArray(PHOTOS),
-      },
-      location,
-    };
+    let advert = createAdvert(i);
     result.push(advert);
   }
 
   return result;
 };
 
-let createPin = (advert) => {
-  let template = document.querySelector(`#pin`).content.querySelector(`.map__pin`).cloneNode(true);
-  let img = template.getElementsByTagName(`img`)[0];
+let createPin = (advert, template) => {
+  let pin = template.cloneNode(true);
+  let img = pin.getElementsByTagName(`img`)[0];
   img.setAttribute(`src`, advert.author.avatar);
   img.setAttribute(`alt`, advert.offer.title);
-  template.setAttribute(`style`, `left: ${advert.location.x}px; top: ${advert.location.y}px`);
+  pin.setAttribute(`style`, `left: ${advert.location.x}px; top: ${advert.location.y}px`);
 
-  return template;
+  return pin;
 };
 
 let createPins = () => {
   let adverts = generateAdverts();
   let fragment = document.createDocumentFragment();
+  let template = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
   adverts.forEach((advert) => {
-    fragment.appendChild(createPin(advert));
+    fragment.appendChild(createPin(advert, template));
   });
 
   return fragment;
